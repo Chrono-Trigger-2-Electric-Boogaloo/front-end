6f2af7a01e38764247b3edc305f6b7a5453c6f27
Julie Antonio <julieantonio38@gmail.com>
2020-02-06 17:27:37 -0500

    changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,10 +2,25 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const LandingPage = () => {
+
     const history = useHistory()
-    useEffect(() => {
-        document.addEventListener('keydown', e => history.push('/index2'))
+    // const nextPage = () => history.push('/index2');
+
+    // useEffect(() => {
+    //     document.addEventListener('keydown', e => {
+    //         document.removeEventListener('keydown', nextPage)
+    //         nextPage()
+    //     })
+    // }, [])
+
+    const eventthing = () =>{
+        document.removeEventListener('keydown', eventthing)
+        history.push('/index2')
+    }
+useEffect(() => {
+        document.addEventListener('keydown', eventthing)
     }, [])
+
     return(
         <div className='landing-page'>
             <img src='./landing.jpeg' />
